74467deb83fea5170ba3ea668835ec5ca7fd7cb9
alexlsalt <am@alexmortoncreative.com>
2021-02-05 11:02:22 +0100

    Initialized repository

---
+++ lib/almorton_palindrome.rb
@@ -0,0 +1,6 @@
+require "almorton_palindrome/version"
+
+module AlmortonPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/almorton_palindrome/version.rb
@@ -0,0 +1,3 @@
+module AlmortonPalindrome
+  VERSION = "0.1.0"
+end
